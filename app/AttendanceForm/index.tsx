86ec24213722167337f5483737806932d787fee8
BradleyExton <bradleyexton@Bradleys-MacBook-Pro.local>
2025-09-08 12:08:19 -0400

    fix batting order rules

--- app/AttendanceForm/index.tsx
+++ app/AttendanceForm/index.tsx
@@ -84,7 +84,7 @@ export default function AttendanceForm({
             isExpanded ? "translate-y-0" : "-translate-y-4"
           }`}
         >
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4">
             {players.map((player) => (
               <label
                 key={player.name}
